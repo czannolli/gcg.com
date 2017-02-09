44f77a17607879425919b8e3f4291d54760f842f
czannolli <carlo.zannolli@gmail.com>
2017-02-09 23:55:18 +0100

    animation

--- js/main.js
+++ js/main.js
@@ -2,19 +2,29 @@ var a =  $('.title');
 var b =  $('.title .part-1');
 var c =  $('.title .part-2');
 var d =  $('.arrow-container');
-var e =  $('img.arrow1');
 var arrows = $('.arrow-container > div');
+var arrowImgs = $('.arrow-container img');
 var arrowsLinks = $('.arrow-container > div > .arrow-links');
-var coorLA = [ '66%', '78%', '6%'];
+var coorLA = [ '70%', '80%', '0%'];
 var coorLB = [ '61%', '72%', '15%' ];
+var zindexLB = [ '5', '3', '1' ];
 
 arrowsLinks.find('div.arl').each(function(){
-	$(this).css('padding-right', ($(this).attr('data-number') * 20) + 'px');
+	$(this).css('margin-right', ($(this).attr('data-number') * 10) + 'px');
+});
+
+d.one('webkitAnimationEnd oanimationend msAnimationEnd animationend',
+  function(e) {
+
 });
 
 $(document).ready(function(){
     move();
-    showArrow();    
+    d.one('webkitAnimationEnd oanimationend msAnimationEnd animationend',
+    function(e) {
+        showArrow();
+    });
+
 });
 
 $(window).on('resize', function(){
@@ -23,21 +33,20 @@ $(window).on('resize', function(){
 
 function move(){
     a.css('height', b.height());
-    d.css('height', e.height()*2);
+    d.css('height', $(window).height() / 100 * 80);
     b.css('position', 'absolute').animate({ left: a.width() - b.width() });
     c.css('position', 'absolute').animate({ left: a.width() - c.width() });
     $('.content > .img-cover').css('top', (($(window).height() - $('.content > .img-cover').height()) / 2) + 'px');
 }
 
 function showArrow(){
-	console.clear();	
 	arrows.hover(function(){
 		let links = $(this).find('.arrow-links');
 		let n = links.attr('data-number') - 1;
-		links.show().animate({ opacity: 1, left: coorLA[n] }, {duration: 500, easing: 'easeOutBounce'});
+		links.show().animate({ opacity: 1, left: coorLA[n] }, {duration: 500, easing: 'easeOutBounce', complete: function(){ $(this).css('z-index', 10); }});
     }, function(){
     	let links = $(this).find('.arrow-links');
 		let n = links.attr('data-number') - 1;
-    	links.animate({ opacity: 0.25, left: coorLB[n] }, {duration: 500, easing: 'easeOutBounce', complete: function(){ $(this).hide().css('left', coorLB[n]); }});
+    	links.css('z-index', zindexLB[n]).animate({ opacity: 0.25, left: coorLB[n] }, {duration: 500, easing: 'easeOutBounce', complete: function(){ $(this).hide().css('left', coorLB[n]); }});
     });
 }
